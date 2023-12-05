112528325e58fa58a5d8dd25f1c8f28d45df44af
CharlyMoraUA <ca.morar1@uniandes.edu.co>
2023-12-04 23:00:10 -0500

    add form complete

--- Services/AnimalService.cs
+++ Services/AnimalService.cs
@@ -1,8 +1,5 @@
-using System;
-using System.Collections.Generic;
 using System.Net.Http.Json;
 using System.Text.Json;
-using System.Threading.Tasks;
 using Microsoft.JSInterop;
 using st_genetics_v1.Models;
 
@@ -13,7 +10,7 @@ namespace st_genetics_v1.Services
         private List<Animal> _animals;
         private readonly IJSRuntime _jsRuntime;
         private readonly HttpClient _httpClient;
-         private readonly string _filePath;
+        private readonly string _filePath;
 
         public AnimalService(HttpClient httpClient, IJSRuntime jsRuntime)
         {
@@ -35,7 +32,7 @@ namespace st_genetics_v1.Services
                 _animals = string.IsNullOrEmpty(json)
                     ? await _httpClient.GetFromJsonAsync<List<Animal>>(_filePath)
                     : JsonSerializer.Deserialize<List<Animal>>(json);
-                
+
             }
             catch (Exception ex)
             {
@@ -45,11 +42,16 @@ namespace st_genetics_v1.Services
 
         public async Task AddAnimalAsync(Animal newAnimal)
         {
-            // Assign a unique ID to the new animal here
-            newAnimal.AnimalId = _animals.Count + 1;
+            // Find the maximum existing AnimalId
+            int maxAnimalId = _animals.Any() ? _animals.Max(a => a.AnimalId) : 0;
+
+            // Assign a unique ID to the new animal
+            newAnimal.AnimalId = maxAnimalId + 1;
+
             _animals.Add(newAnimal);
             await SaveChangesToFileAsync();
         }
+
 
         public async Task UpdateAnimalAsync(Animal updatedAnimal)
         {
@@ -59,6 +61,22 @@ namespace st_genetics_v1.Services
             {
                 _animals[index] = updatedAnimal;
                 await SaveChangesToFileAsync();
+            }
+        }
+
+        public async Task UpdateAnimalPropertyAsync(int animalId, string propertyName, string propertyValue)
+        {
+            // Update a specific property of the animal
+            var animalToUpdate = _animals.Find(a => a.AnimalId == animalId);
+            if (animalToUpdate != null)
+            {
+                // Use reflection to set the property value dynamically
+                var propertyInfo = typeof(Animal).GetProperty(propertyName);
+                if (propertyInfo != null)
+                {
+                    propertyInfo.SetValue(animalToUpdate, Convert.ChangeType(propertyValue, propertyInfo.PropertyType));
+                    await SaveChangesToFileAsync();
+                }
             }
         }
 
