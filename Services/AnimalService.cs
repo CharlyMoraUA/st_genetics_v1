716aa6cd1a3e213488f19220ad71710eb67ce058
CharlyMoraUA <ca.morar1@uniandes.edu.co>
2023-12-04 21:26:48 -0500

    saving information

--- Services/AnimalService.cs
+++ Services/AnimalService.cs
@@ -1,4 +1,9 @@
+using System;
+using System.Collections.Generic;
 using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
 using st_genetics_v1.Models;
 
 namespace st_genetics_v1.Services
@@ -6,13 +11,15 @@ namespace st_genetics_v1.Services
     public class AnimalService
     {
         private List<Animal> _animals;
-        private readonly string _filePath;
+        private readonly IJSRuntime _jsRuntime;
         private readonly HttpClient _httpClient;
+         private readonly string _filePath;
 
-        public AnimalService(HttpClient httpClient)
+        public AnimalService(HttpClient httpClient, IJSRuntime jsRuntime)
         {
-            _filePath = Path.Combine(AppContext.BaseDirectory, "sample-data/animaldata.json");
+            _jsRuntime = jsRuntime;
             _httpClient = httpClient;
+            _filePath = Path.Combine(AppContext.BaseDirectory, "sample-data/animaldata.json");
         }
 
         public List<Animal> GetAnimals()
@@ -20,15 +27,15 @@ namespace st_genetics_v1.Services
             return _animals;
         }
 
-
         public async Task LoadAnimalsAsync()
         {
             try
             {
-                _animals = await _httpClient.GetFromJsonAsync<List<Animal>>(_filePath);
-                Console.WriteLine("_animals");
-                Console.WriteLine(_animals);
-                Console.WriteLine("_animals");
+                var json = await _jsRuntime.InvokeAsync<string>("localStorage.getItem", "animals");
+                _animals = string.IsNullOrEmpty(json)
+                    ? await _httpClient.GetFromJsonAsync<List<Animal>>(_filePath)
+                    : JsonSerializer.Deserialize<List<Animal>>(json);
+                
             }
             catch (Exception ex)
             {
@@ -36,5 +43,41 @@ namespace st_genetics_v1.Services
             }
         }
 
+        public async Task AddAnimalAsync(Animal newAnimal)
+        {
+            // Assign a unique ID to the new animal here
+            newAnimal.AnimalId = _animals.Count + 1;
+            _animals.Add(newAnimal);
+            await SaveChangesToFileAsync();
+        }
+
+        public async Task UpdateAnimalAsync(Animal updatedAnimal)
+        {
+            // Update the animal in the list
+            int index = _animals.FindIndex(a => a.AnimalId == updatedAnimal.AnimalId);
+            if (index != -1)
+            {
+                _animals[index] = updatedAnimal;
+                await SaveChangesToFileAsync();
+            }
+        }
+
+        public async Task DeleteAnimalAsync(int animalId)
+        {
+            _animals.RemoveAll(a => a.AnimalId == animalId);
+            await SaveChangesToFileAsync();
+        }
+
+        private async Task SaveChangesToFileAsync()
+        {
+            try
+            {
+                await _jsRuntime.InvokeVoidAsync("localStorage.setItem", "animals", JsonSerializer.Serialize(_animals));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error saving changes to localStorage: {ex.Message}");
+            }
+        }
     }
 }
